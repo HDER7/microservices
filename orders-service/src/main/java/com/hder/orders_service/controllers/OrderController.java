3d59bf72d3f72e96fad0869aa6bc466cfb36aa43
Heider Rivas <heiderarb@gmail.com>
2024-07-16 09:10:02 -0500

    notification-service and kafka added

--- orders-service/src/main/java/com/hder/orders_service/controllers/OrderController.java
+++ orders-service/src/main/java/com/hder/orders_service/controllers/OrderController.java
@@ -3,6 +3,7 @@ package com.hder.orders_service.controllers;
 import com.hder.orders_service.model.dtos.OrderRequest;
 import com.hder.orders_service.model.dtos.OrderResponse;
 import com.hder.orders_service.services.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "orders-service", fallbackMethod = "placeOrderFallback")
     public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest order, UriComponentsBuilder uriBuilder) {
         var o = orderService.placeOrder(order);
         URI url = uriBuilder.path("/api/order/{id}").buildAndExpand(o.id()).toUri();
@@ -30,5 +32,9 @@ public class OrderController {
     @GetMapping
     public ResponseEntity<List<OrderResponse>> getAllOrders() {
         return ResponseEntity.ok(orderService.getAllOrders());
+    }
+
+    private ResponseEntity<OrderResponse> placeOrderFallback(@RequestBody OrderRequest order,Throwable throwable) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
     }
 }
